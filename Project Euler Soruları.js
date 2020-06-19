86ff8758d9e2ac8e7858755d85957121d6913e0c
Berke Karabulut <63062558+Berke-Karabulut@users.noreply.github.com>
2020-06-20 00:32:50 +0300

    Update Project Euler Soruları.js

--- Project Euler Soruları.js
+++ Project Euler Soruları.js
@@ -3,7 +3,7 @@
 /*
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-Find the sum of all the primes below two million.
+Find the sum of all the primes below two million..
 */
 
 
@@ -27,6 +27,7 @@ function asalFactors (number){
 }
 
 let getAllAsallar = []
+
 i=2
 while (i<2000000){
     if (asalFactors(i).length == 1){
